1351db26c9422048d9691b0d8585bdce3c492cbd
Harisu Fanyui <fanyuiharisu@gmail.com>
2019-01-04 15:15:41 +0100

    Added wildcard exclusion of some urls for csrf verfication

--- app/Http/Middleware/VerifyCsrfToken.php
+++ app/Http/Middleware/VerifyCsrfToken.php
@@ -12,6 +12,6 @@ class VerifyCsrfToken extends Middleware
      * @var array
      */
     protected $except = [
-        	'http://finvat.com/account/fitnance/callback',
+        	'http://finvat.com/account/fitnance/*',
     ];
 }
