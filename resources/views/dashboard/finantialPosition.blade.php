749aa516385414fb5d06421b52df4bffec6e11f1
Harisu Fanyui <fanyuiharisu@gmail.com>
2019-01-26 05:46:48 +0100

    Added the tab name financial position when the financtial position tab is active

--- resources/views/dashboard/finantialPosition.blade.php
+++ resources/views/dashboard/finantialPosition.blade.php
@@ -4,7 +4,7 @@
 <!-- Navbar -->
             <nav class="navbar navbar-expand-lg " color-on-scroll="500">
                 <div class=" container-fluid  ">
-                    <a class="navbar-brand" href="{{route('home')}}"> CUENTAS </a>
+                    <a class="navbar-brand" href="{{route('home')}}">       POSICIÓN FINANCIERA </a>
                     <button href="" class="navbar-toggler navbar-toggler-right" type="button" data-toggle="collapse" aria-controls="navigation-index" aria-expanded="false" aria-label="Toggle navigation">
                         <span class="navbar-toggler-bar burger-lines"></span>
                         <span class="navbar-toggler-bar burger-lines"></span>
